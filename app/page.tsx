41ac8cd97ffc3d6cdd441f2f828495cbc49f129f
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-12-18 17:59:48 +0700

    style: still testing a button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Button>
+    <Button size="lg" variant="destructive"> 
       Click me
     </Button>
   )
