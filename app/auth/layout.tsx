fae205802e5b7024f3aa29d14ea556b86fff98d8
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-12-19 11:47:32 +0700

    Testing navbar layout for auth page

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,8 +6,11 @@ export default function AuthPage({
     children: React.ReactNode
 }) {
     return (
-        <>
+        <div className="flex flex-col gap-y-4">
+            <nav className="bg-red-500 w-full p-1">
+                Auth navbar
+            </nav>
             {children}
-        </>
+        </div>
     )
 }
